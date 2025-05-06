a54620c2a1d4483fa1bf6b74accd43a3b43eb851
Hob Nguyen <hoangnh@vmodev.com>
2025-05-06 16:18:27 +0700

    increase version and change google api key

--- example/lib/utils/multi-languages/locale_keys.dart
+++ example/lib/utils/multi-languages/locale_keys.dart
@@ -5,7 +5,7 @@ part "locale_keys.g.dart";
 
 @SheetLocalization(
   docId: '1WnE6x_JMVP3JXL88Q2BLbvfZ5X9fMV9wOQh5LjykZ0U',
-  apiKey: 'AIzaSyD6RsxgI1Ttu9JrmwrCnEjCGYONxwN-4Pc',
+  apiKey: 'YOUR_GOOGLE_API_READ_KEY',
   version: 1,
 )
 class _LocaleKeys {}
